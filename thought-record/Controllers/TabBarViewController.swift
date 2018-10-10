7210399100101935bdb213e3bf62ca07c50b62a7
Leeann Drees <leeann.drees@detroitlabs.com>
2018-10-10 11:02:03 -0400

    tab bar setup

---
+++ thought-record/Controllers/TabBarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarViewController.swift
+//  thought-record
+//
+//  Created by DetroitLabs on 10/10/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}

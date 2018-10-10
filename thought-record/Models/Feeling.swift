32e3b652b107968766804aa72bb4dabb78d0cac6
Leeann Drees <leeann.drees@detroitlabs.com>
2018-10-10 11:11:22 -0400

    creating simple Feeling class (lol)

---
+++ thought-record/Models/Feeling.swift
@@ -0,0 +1,14 @@
+//
+//  Feeling.swift
+//  thought-record
+//
+//  Created by DetroitLabs on 10/10/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+import Foundation
+
+class Feeling: Codable {
+    var name: String
+    var rating: Int
+}
---
+++ thought-record/Models/ThoughtRecord.swift
@@ -0,0 +1,9 @@
+//
+//  ThoughtRecord.swift
+//  thought-record
+//
+//  Created by DetroitLabs on 10/10/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+import Foundation

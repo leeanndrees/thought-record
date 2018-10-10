25313d4a2072abc3965df52f9d74330993e5b1db
Leeann Drees <leeann.drees@detroitlabs.com>
2018-10-10 14:16:01 -0400

    finding out date of last entry and displaying it

--- thought-record/Controllers/HomeViewController.swift
+++ thought-record/Controllers/HomeViewController.swift
@@ -10,12 +10,31 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    // MARK: Properties
+    var lastRecord: ThoughtRecord?
+    
+    // MARK: Outlets
     @IBOutlet weak var welcomeLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var encouragementLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getLastRecord()
+        updateLabels()
+    }
+    
+    func getLastRecord() {
+        let database = ThoughtRecordDatabase()
+        lastRecord = database.thoughts.last
     }
 
+    func updateLabels() {
+        if let lastTime = lastRecord?.date {
+            timeLabel.text = "Your last check-in was on \(lastTime)."
+        } else {
+            timeLabel.text = "Nice to see you again!"
+        }
+    }
+    
 }

34498d1f00594ca19d03959cf84a5e0b38287fd1
Leeann Drees <leeann.drees@detroitlabs.com>
2018-10-10 11:20:50 -0400

    setting up ThoughtRecord class with many many properties 😬

--- thought-record/Models/ThoughtRecord.swift
+++ thought-record/Models/ThoughtRecord.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+class ThoughtRecord: Codable {
+    
+    var date: String
+    var thought: String
+    var situation: String
+    
+    var feelingsStart: [Feeling]
+    var unhelpfulThoughts: String
+    var factsSupporting: String
+    var factsAgainst: String
+    var balancedPerspective: String
+    var feelingsEnd: [Feeling]
+    
+    var tags: [Tag]
+    
+    init(date: String, thought: String, situation: String, feelingsStart: [Feeling], unhelpfulThoughts: String, factsSupporting: String, factsAgainst:String, balancedPerspective: String, feelingsEnd: [Feeling], tags: [Tag]) {
+        self.date = date
+        self.thought = thought
+        self.situation = situation
+        self.feelingsStart = feelingsStart
+        self.unhelpfulThoughts = unhelpfulThoughts
+        self.factsSupporting = factsSupporting
+        self.factsAgainst = factsAgainst
+        self.balancedPerspective = balancedPerspective
+        self.feelingsEnd = feelingsEnd
+        self.tags = tags
+    }
+}

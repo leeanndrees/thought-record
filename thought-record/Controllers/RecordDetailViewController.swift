1560506a51e228246125045b9656cceb9c4e1ab1
Leeann Drees <leeann.drees@detroitlabs.com>
2018-10-10 15:09:55 -0400

    creating & connecting view controller file

---
+++ thought-record/Controllers/RecordDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RecordDetailViewController.swift
+//  thought-record
+//
+//  Created by DetroitLabs on 10/10/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+import UIKit
+
+class RecordDetailViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+   
+}

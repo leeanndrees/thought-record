ba01d538ec69a7f78a17373c821f2adc0c3855bc
Leeann Drees <leeann.drees@detroitlabs.com>
2018-10-10 15:14:15 -0400

    connecting all labels to outlets

--- thought-record/Controllers/RecordDetailViewController.swift
+++ thought-record/Controllers/RecordDetailViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class RecordDetailViewController: UITableViewController {
-
+    
+    // MARK: Outlets
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var thoughtLabel: UILabel!
+    @IBOutlet weak var situationLabel: UILabel!
+    @IBOutlet weak var feelingsStartLabel: UILabel!
+    @IBOutlet weak var unhelpfulThoughtsLabel: UILabel!
+    @IBOutlet weak var factsSupportingLabel: UILabel!
+    @IBOutlet weak var factsAgainstLabel: UILabel!
+    @IBOutlet weak var balancedPerspectiveLabel: UILabel!
+    @IBOutlet weak var feelingsEndLabel: UILabel!
+    @IBOutlet weak var tagsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }

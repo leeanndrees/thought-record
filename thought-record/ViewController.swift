b5653a01f2ea724122d52bae797236899407e1db
Leeann Drees <leeann.drees@detroitlabs.com>
2018-10-10 09:15:56 -0400

    initial commit

---
+++ thought-record/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  thought-record
+//
+//  Created by DetroitLabs on 10/10/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

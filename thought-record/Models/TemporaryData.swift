3f6e7388ae72bc841a00dcf35cf0c802e3275648
Leeann Drees <leeann.drees@detroitlabs.com>
2018-10-11 09:27:43 -0400

    adding more details to temporary data

--- thought-record/Models/TemporaryData.swift
+++ thought-record/Models/TemporaryData.swift
@@ -13,7 +13,8 @@ class FeelingDatabase {
     var feelings = [
         Feeling(name: "excited", rating: 8),
         Feeling(name: "scared", rating: 6),
-        Feeling(name: "relaxed", rating: 5)
+        Feeling(name: "relaxed", rating: 5),
+        Feeling(name: "worried", rating: 3)
     ]
     
 }
@@ -33,13 +34,13 @@ class ThoughtRecordDatabase {
     
     var thoughts = [
         ThoughtRecord(date: "October 10",
-                      thought: "I'm never going to finish my capstone!",
+                      thought: "I'm never going to finish my capstone! It's too much.",
                       situation: "Working on capstone",
-                      feelingsStart: [FeelingDatabase().feelings[0], FeelingDatabase().feelings[1]],
-                      unhelpfulThoughts: "oh no",
-                      factsSupporting: "It's hard and ambitious",
-                      factsAgainst: "I am smart. I can pare down the project to a reasonable MVP.",
-                      balancedPerspective: "I will finish",
+                      feelingsStart: [FeelingDatabase().feelings[1], FeelingDatabase().feelings[3]],
+                      unhelpfulThoughts: "Everyone else has a better idea than me and I've somehow forgotten everything I ever learned about iOS devlepment and I'm going to get fired omg.",
+                      factsSupporting: "It's a difficult and ambitious project and program!",
+                      factsAgainst: "I've done well on the projects so far, even when they were overwhelming. I can pare down this project to a reasonable MVP.",
+                      balancedPerspective: "I will finish. Even if I don't get to every feature I want, I'll have something to show that will demonstrate how far I've come in just 12 weeks.",
                       feelingsEnd: [FeelingDatabase().feelings[0]],
                       tags: [TagDatabase().tags[0]]
     ),

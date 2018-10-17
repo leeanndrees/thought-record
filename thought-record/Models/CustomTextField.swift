69d24a89da09d849b221e77d7ed331573df14da1
Leeann Drees <leeann.drees@detroitlabs.com>
2018-10-17 17:44:01 -0400

    fields are now white

---
+++ thought-record/Models/CustomTextField.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTextField.swift
+//  thought-record
+//
+//  Created by DetroitLabs on 10/17/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomTextField: UITextField {
+    
+    let padding = UIEdgeInsets(top: 15, left: 0, bottom: 0, right: 0);
+    
+    var paddingView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 100.0, height: 2.0))
+    
+}

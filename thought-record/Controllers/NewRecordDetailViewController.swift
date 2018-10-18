55d2b2b1205068e3c17b54266f9e6fcf17e05fcb
Leeann Drees <leeann.drees@detroitlabs.com>
2018-10-18 16:21:00 -0400

    first edit + view views in place

--- thought-record/Controllers/NewRecordDetailViewController.swift
+++ thought-record/Controllers/NewRecordDetailViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class NewRecordDetailViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+ 
     
 
     /*

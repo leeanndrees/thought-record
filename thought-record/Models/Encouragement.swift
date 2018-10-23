fc4269b6c3d04e4bc5593e4f72b4d7a73ef59bcc
Leeann Drees <leeann.drees@detroitlabs.com>
2018-10-23 14:49:29 -0400

    randomized Encouragement (mostly emoji hearts lol)

---
+++ thought-record/Models/Encouragement.swift
@@ -0,0 +1,15 @@
+//
+//  Encouragement.swift
+//  thought-record
+//
+//  Created by DetroitLabs on 10/23/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+import Foundation
+
+let encouragements = [
+    "Today is a great day to check in & untangle a thought. :)",
+    "💜",
+    "🖤"
+]

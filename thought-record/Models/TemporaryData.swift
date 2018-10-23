cb8a94f9704b806a2b64a3905be95477c602d0f5
Leeann Drees <leeann.drees@detroitlabs.com>
2018-10-23 16:10:47 -0400

    rewriting sample record, removing Tag & Feeling databases

--- thought-record/Models/TemporaryData.swift
+++ thought-record/Models/TemporaryData.swift
@@ -8,50 +8,19 @@
 
 import Foundation
 
-class FeelingDatabase {
-    
-    var feelings = [
-        Feeling(name: "excited", rating: 8),
-        Feeling(name: "scared", rating: 6),
-        Feeling(name: "relaxed", rating: 5),
-        Feeling(name: "worried", rating: 3)
-    ]
-    
-}
-
-class TagDatabase {
-    
-    var tags = [
-        Tag(name: "work"),
-        Tag(name: "friends"),
-        Tag(name: "weather: cloudy")
-    ]
-    
-}
-
 class ThoughtRecordDatabase {
     
     var thoughts = [
         ThoughtRecord(date: Date(),
-                      thought: "I'm never going to finish my capstone! It's too much.",
-                      situation: "Working on capstone",
-                      feelingsStart: [FeelingDatabase().feelings[1], FeelingDatabase().feelings[3]],
-                      unhelpfulThoughts: "Everyone else has a better idea than me and I've somehow forgotten everything I ever learned about iOS devlepment and I'm going to get fired omg.",
-                      factsSupporting: "It's a difficult and ambitious project and program!",
-                      factsAgainst: "I've done well on the projects so far, even when they were overwhelming. I can pare down this project to a reasonable MVP.",
-                      balancedPerspective: "I will finish. Even if I don't get to every feature I want, I'll have something to show that will demonstrate how far I've come in just 12 weeks.",
-                      feelingsEnd: [FeelingDatabase().feelings[0]],
-                      tags: [TagDatabase().tags[0]]
-    ),
-        ThoughtRecord(date: Date(),
-                      thought: "I'm out sick so I'm going to forget everything I ever learned about coding",
-                      situation: "home sick from work",
-                      feelingsStart: [FeelingDatabase().feelings[0]],
-                      unhelpfulThoughts: "Everyone else is getting time to work",
-                      factsSupporting: "I am missing some work time",
-                      factsAgainst: "I will be able to make it up. We have a long time to work on these projects.",
-                      balancedPerspective: "I need to rest up because I'm sick. I'll get back to work tomorrow & it'll be ok.",
-                      feelingsEnd: [FeelingDatabase().feelings[2]],
-                      tags: [TagDatabase().tags[0]])
+                      thought: "Sample: presenting this app at work",
+                      situation: "work presentation",
+                      feelingsStart: [Feeling(name: "nervous", rating: 8)],
+                      unhelpfulThoughts: "I'll probably throw up and die.",
+                      factsSupporting: "Public speaking can make me nervous.",
+                      factsAgainst: "I have done public speaking in the past, without throwing up or dying. Sometimes it's even been fun and rewarding.",
+                      balancedPerspective: "I've worked hard to present this well, and I'm proud of the project as a whole. If it doesn't go well, I'll still be ok.",
+                      feelingsEnd: [Feeling(name: "calmer", rating: 5)],
+                      tags: [Tag(name: "work"), Tag(name: "public speaking")]
+    )
     ]
 }
